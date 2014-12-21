47674065615ce1415c06a26b07a295ea0ac8e2a9
acaballero2010 <acaballero2010@yahoo.com>
2014-12-21 17:00:58 +0800

    added new lectures on data analysis 3 file

--- data_analysis_3.R
+++ data_analysis_3.R
@@ -14,3 +14,57 @@ StarWars = data.frame(
 )
 
 View(StarWars)
+
+# Modifying StarWars
+# affecting cell 1,1
+StarWars[1,1]
+
+# Gender as factor
+StarWars[ ,2] = factor(c('male', 'female', 'male', 'female'))
+
+# Modifyig columns using names
+# name in upper case
+StarWars[ ,"Name"] = toupper(StarWars[ ,"Name"])
+View(StarWars)
+
+# Modifying one column using $
+# changing jedi column
+StarWars$Jedi <- paste(StarWars$Jedi, 
+                       'jedi', sep = '_')
+
+View(StarWars)
+
+# 1st column (lower case names)
+StarWars[[1]] = tolower(StarWars$Name)
+View(StarWars)
+
+# column Born (removing BBY)
+StarWars[["Born"]] = gsub('BBY', '', StarWars$Born)
+View(StarWars)
+
+# changing male names
+StarWars[StarWars$Gender == 'male', 'Name'] <- c('Anakin', 'Luke')
+View(StarWars)
+
+# multiple conditions
+cond <- (StarWars$Gender == 'female' & StarWars$Homeland != 'Alderaan')
+StarWars[cond, 'Name'] <- 'PADME'
+View(StarWars)
+
+## BINDING COLUMNS AND ROWS
+# Adding new column with cbind()
+#number of rows
+num_obs = nrow(StarWars)
+
+# add random column
+StarWars = cbind(StarWars, x = runif(num_obs))
+View(StarWars)
+
+# Adding new row with rbind()
+# adding records for Obi-Wan
+
+
+
+
+
+
